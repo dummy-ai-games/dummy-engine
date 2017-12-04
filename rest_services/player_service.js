238130207725bd78e5d89f529dff385e603a8aa5
pingding <905613352@qq.com>
2017-12-04 21:24:43 +0800

    add encryption password.

--- rest_services/player_service.js
+++ rest_services/player_service.js
@@ -6,13 +6,12 @@ var logger = require('../poem/logging/logger4js').helper;
 var PlayerResponse = require('../responses/player_response');
 var playerLogic = require('../work_units/player_logic');
 var jwt = require('jsonwebtoken');
-
+var encrypt = require('../poem/crypto/encrypt');
 var ErrorCode = require('../constants/error_code.js');
 var errorCode = new ErrorCode();
-
 const JWT_SECRET = 'dummy_ymmub';
 
-exports.signup = function (req,res){
+exports.signup = function (req, res) {
     var phoneNumber = req.body.phoneNumber;
     var pwd = req.body.password;
     var name = req.body.username;
@@ -20,50 +19,54 @@ exports.signup = function (req,res){
     var user = {
         name: name,
         phoneNumber: phoneNumber,
-        password: pwd,
-        avatar:avatar,
-        role:0, // need to consider different roles
-        status:0
+        password: encrypt.encryptStr(pwd),
+        avatar: avatar,
+        role: 0, // need to consider different roles
+        status: 0
     };
 
     var playerResponse = new PlayerResponse();
-    playerLogic.registerWorkUnit(user, function(registerErr){
+    playerLogic.registerWorkUnit(user, function (registerErr) {
         playerResponse.status = registerErr;
-        if(registerErr !== errorCode.SUCCESS.code){
+        if (registerErr !== errorCode.SUCCESS.code) {
             // 注册失败
-            res.send(userResponse);
+            playerResponse.entity = null;
+            res.send(playerResponse);
             return res.end();
         }
         // 注册成功即登录
         // jwt session
         delete user.password;
         var token = jwt.sign(user, JWT_SECRET);
-        playerResponse.entity = { token, user };
+        logger.info(token);
+        logger.info(user);
+        playerResponse.entity = {token, user};
         res.send(playerResponse);
         res.end();
     });
 };
 
 
-exports.login = function(req, res){
+exports.login = function (req, res) {
     var phoneNumber = req.body.phoneNumber;
     var pwd = req.body.password;
     var user = {
         phoneNumber: phoneNumber,
-        password: pwd
+        password: encrypt.encryptStr(pwd)
     };
     var playerResponse = new PlayerResponse();
-    playerLogic.getPlayerWorkUnit(user, function(getUserErr, users){
+    playerLogic.getPlayerWorkUnit(user, function (getUserErr, users) {
         playerResponse.status = getUserErr;
-        if(getUserErr !== errorCode.SUCCESS.code){
+        if (getUserErr !== errorCode.SUCCESS.code) {
             // 登录失败
+            playerResponse.entity = null;
             res.send(playerResponse);
             return res.end();
         }
         user = users[0];
         delete user.password;
         var token = jwt.sign(user, JWT_SECRET);
-        playerResponse.entity = { token, user };
+        playerResponse.entity = {token, user};
         res.send(playerResponse);
         res.end();
     });
